9ac5952395f9682122f9335704cf64b1c723a721
javia <javia@192.168.1.216>
2026-02-05 21:43:36 +0100

    Segundo commit: modificación del mensaje de salida

--- src/com/dam/git/HolaGit.java
+++ src/com/dam/git/HolaGit.java
@@ -3,7 +3,8 @@ package com.dam.git;
 public class HolaGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hola desde Git");
+		System.out.println("Hola desde Git - versión modificada");
+        System.out.println("Aprendiendo control de versiones");
 
 	}
 
